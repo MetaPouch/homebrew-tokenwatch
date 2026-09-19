c7a7676a7485cc39bad4235cbd68d89743c98ef8
Ajay Srinivas <20982682+ajays97@users.noreply.github.com>
2026-09-19 11:14:39 +0530

    Add tokenwatch cask

---
+++ Casks/tokenwatch.rb
@@ -0,0 +1,21 @@
+cask "tokenwatch" do
+  version "1.0.0"
+  sha256 "d88da1db63116e3840531e74f0b8bca7f4badb7adb8c7e01fa03edb14a5b9c15"
+
+  url "https://github.com/ajays97/tokenwatch/releases/download/v#{version}/TokenWatch-#{version}.dmg"
+  name "TokenWatch"
+  desc "Menu-bar usage tracker for AI subscriptions, routing providers, and API keys"
+  homepage "https://tokenwatch.fyi"
+
+  auto_updates false
+  depends_on macos: ">= :sonoma"
+
+  app "TokenWatch.app"
+
+  zap trash: [
+    "~/Library/Application Support/TokenWatch",
+    "~/Library/Caches/dev.tokenwatch.TokenWatch",
+    "~/Library/Preferences/dev.tokenwatch.TokenWatch.plist",
+    "~/Library/Saved Application State/dev.tokenwatch.TokenWatch.savedState",
+  ]
+end
